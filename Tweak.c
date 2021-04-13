a09f8a5ab6f63d77031a9b7c92ebdb701e63f760
Ryan Nair <ryannair05@gmail.com>
2021-04-13 08:25:42 -0400

    Revert changes to preference file in a more reliable way

--- Tweak.c
+++ Tweak.c
@@ -1,10 +1,11 @@
 #import <CoreFoundation/CFNumber.h>
 #include <CoreFoundation/CFPreferences.h>
-
-static __attribute__((destructor)) void deInit() {
-    CFPreferencesSetAppValue(CFSTR("SBDontLockAfterCrash"), kCFBooleanFalse, CFSTR("com.apple.springboard"));
-}
+#include <Dispatch/queue.h>
 
 static __attribute__((constructor)) void Init() {
     CFPreferencesSetAppValue(CFSTR("SBDontLockAfterCrash"), kCFBooleanTrue, CFSTR("com.apple.springboard"));
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 7.5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        CFPreferencesSetAppValue(CFSTR("SBDontLockAfterCrash"), kCFBooleanFalse, CFSTR("com.apple.springboard"));
+    });
 }
